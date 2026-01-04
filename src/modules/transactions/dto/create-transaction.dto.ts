f2ccc7daf961d08224b9fb3536e5be35ae4c6ca2
murilo rocha <murilorocha537@gmail.com>
2026-01-03 22:38:07 -0300

    feat:swagger for dto transaction

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,9 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsISO8601, IsNumber, Min } from 'class-validator';
 
 export class CreateTransactionDto {
+  @ApiProperty({
+    example: 100.5,
+    description: 'Valor da transação',
+  })
   @IsNumber()
   @Min(0)
   amount: number;
+
+  @ApiProperty({
+    example: '2026-01-25T12:00:00Z',
+    description: 'Timestamp da transação',
+  })
   @IsISO8601({ strict: true }) //strict obriga tudo
   timestamp: string;
 }
